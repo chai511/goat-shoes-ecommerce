f9c214e52c0635acccf2fa530d206e3243e92761
Chaithanya Burra <chaithanyabsrk@gmail.com>
2023-06-07 19:09:29 +0530

    microblogging site code

---
+++ build/precache-manifest.c7e63df33e5aaeb19e2f24b613109b11.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "cdad67280e7c5a3dc2d9",
+    "url": "/static/css/main.48db270d.chunk.css"
+  },
+  {
+    "revision": "cdad67280e7c5a3dc2d9",
+    "url": "/static/js/main.d3526270.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a31692164c26607f459a",
+    "url": "/static/css/2.4bd5c3b2.chunk.css"
+  },
+  {
+    "revision": "a31692164c26607f459a",
+    "url": "/static/js/2.7334ad33.chunk.js"
+  },
+  {
+    "revision": "ed367f4f1fd992b9fd54bd95aebbc594",
+    "url": "/static/media/goat_banner.ed367f4f.png"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "7edfaf01ba88bdd857872682a0c7b196",
+    "url": "/index.html"
+  }
+];
